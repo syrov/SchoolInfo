229883745c2e93abe343c82fec12f7c9570d2546
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2011-10-09 21:04:08 +0400

    yalets

---
+++ src/java/ru/compscicenter/schoolinfo/gui/yalets/UserQueryYalet.java
@@ -0,0 +1,19 @@
+package ru.compscicenter.schoolinfo.gui.yalets;
+
+import net.sf.xfresh.core.InternalRequest;
+import net.sf.xfresh.core.InternalResponse;
+import net.sf.xfresh.db.AbstractDbYalet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alex
+ * Date: 09.10.11
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserQueryYalet extends AbstractDbYalet {
+
+    public void process(InternalRequest req, InternalResponse res) {
+
+    }
+}
