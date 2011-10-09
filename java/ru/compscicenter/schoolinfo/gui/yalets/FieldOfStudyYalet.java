e35f65fc5dc93fb826235311077ff8e480279320
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2011-10-09 20:23:43 +0400

    yalet templates added

---
+++ java/ru/compscicenter/schoolinfo/gui/yalets/FieldOfStudyYalet.java
@@ -0,0 +1,11 @@
+package ru.compscicenter.schoolinfo.gui.yalets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alex
+ * Date: 09.10.11
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class FieldOfStudyYalet {
+}
