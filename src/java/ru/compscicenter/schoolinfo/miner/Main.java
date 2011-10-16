112429252cd14fa90ae393b6257ac2c28f11053d
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2011-10-16 17:11:39 +0400

    miner

--- src/java/ru/compscicenter/schoolinfo/miner/Main.java
+++ src/java/ru/compscicenter/schoolinfo/miner/Main.java
@@ -1,23 +1,24 @@
 package ru.compscicenter.schoolinfo.miner;
 
-import java.io.File;
+import org.webharvest.definition.ScraperConfiguration;
+import org.webharvest.runtime.Scraper;
+
 import java.io.IOException;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		
-		String mainDirectory;
-		
-		if(args.length == 1){
-			mainDirectory = args[0].toString();
-		}
-		else{
-			System.out.println("Please specify an input directory");
-			return;
-		}
-		
-		//Connect to DB
+
+    public static void main(String[] args) throws IOException {
+
+        String mainDirectory;
+
+        if (args.length == 1) {
+            mainDirectory = args[0].toString();
+        } else {
+            System.out.println("Please specify an input directory");
+            return;
+        }
+
+        //Connect to DB
 //		Database.connectToDirtyBase();
 //
 //		//System.out.println(Database.getAllCities().size());
@@ -34,8 +35,14 @@ public class Main {
 //			myScrap.minerStart();
 //
 //		 }
-		
-	}
+
+        String configFilePath = " /Users/alex/SchoolInfo/src/topcoder.xml";
+        ScraperConfiguration config = new ScraperConfiguration(configFilePath);
+        TopCoderListener listener = new TopCoderListener();
+        Scraper scraper = new Scraper(config, ".");
+        scraper.addRuntimeListener(listener);
+        scraper.execute();
+    }
 }
 	
 	
---
+++ src/java/ru/compscicenter/schoolinfo/miner/TopCoderListener.java
@@ -0,0 +1,78 @@
+package ru.compscicenter.schoolinfo.miner;
+
+import org.webharvest.runtime.Scraper;
+import org.webharvest.runtime.ScraperRuntimeListener;
+import org.webharvest.runtime.processors.BaseProcessor;
+import org.webharvest.runtime.variables.Variable;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pritykovskaya
+ * Date: 16.10.11
+ * Time: 15:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class TopCoderListener implements ScraperRuntimeListener {
+
+    public void onExecutionStart(Scraper scraper) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onExecutionPaused(Scraper scraper) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onExecutionContinued(Scraper scraper) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onNewProcessorExecution(Scraper scraper, BaseProcessor baseProcessor) {
+        if ("empty".equalsIgnoreCase(scraper.getRunningProcessor().getElementDef().getShortElementName())) {
+            /*           Variable universityName = (Variable) scraper.getContext().get("universityName");
+
+
+     String sUniversityName = clearString(universityName);
+     System.out.println(sUniversityName);       */
+        }
+    }
+
+    public void onExecutionEnd(Scraper scraper) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onProcessorExecutionFinished(Scraper scraper, BaseProcessor baseProcessor, Map map) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onExecutionError(Scraper scraper, Exception e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private String clearString(Variable incomed) {
+
+        if (incomed != null) {
+            if (incomed.toString().equals(""))
+                return null;
+            String toClear = incomed.toString();
+            int a, b;
+            while (toClear.indexOf('<') != (-1)) {
+                a = toClear.indexOf('<');
+                b = toClear.indexOf('>');
+                if (b > a) {
+                    toClear = toClear.substring(0, a) + toClear.substring(b, toClear.length());
+                    toClear = toClear.replaceFirst(">", " ");
+                }
+            }
+            toClear = toClear.replaceAll("\n", " ");
+            toClear = toClear.replaceAll("\t", "");
+            toClear = toClear.replaceAll(" {2,}", " ");
+            toClear = toClear.replaceAll(" {1,}[.]", ".");
+            toClear = toClear.replaceAll(" {1,}[,]", ",");
+            return toClear;
+        } else
+            return null;
+    }
+
+}
